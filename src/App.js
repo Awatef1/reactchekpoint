324e8aafe2db7a47af7f6ad8c4440c82be42a917
kadri Awatef <awatefkadri@yahoo.com>
2021-07-18 22:42:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {Button, Form, FormGroup, FormLabel, FormText, FormCheck, FormControl, Row, Col}from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+      
+          <div className="container">
+            <h1 className="text-center"> WELCOME </h1>
+          </div>
+     
+      <div className="container">
+          <Form>
+            <Row >
+                <Col md>
+                <Form.Label>First name</Form.Label>
+                <Form.Control placeholder="First name" />
+                </Col>
+                <Col md >
+                <Form.Label>Last name</Form.Label>
+                <Form.Control placeholder="Last name" />
+                </Col>
+            </Row>
+      
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Example@gmail.com" />
+              <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+              </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGridAddress">
+            <Form.Label>Address</Form.Label>
+            <Form.Control type="Address" placeholder="Nabeul 8000" />
+          </Form.Group>
+      <Row>
+      <Col md xs={6}>
+      <Form.Label>City</Form.Label>
+        <Form.Control placeholder="City" />
+      </Col>
+      <Col md>
+      <Form.Label>State</Form.Label>
+        <Form.Control placeholder="State" />
+      </Col>
+      <Col md >
+      <Form.Label> zip </Form.Label>
+        <Form.Control placeholder="Zip" />
+      </Col>
+  </Row>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button className="btn-log btn-dark btn-black">
+  Submit
+  </Button>
+  
+</Form>
+
+
     </div>
+    
+</div>
   );
 }
 
